aac49358fb00e8366e84e33d7b00ce0fe02b89c0
aszurita <nzuritag@fiec.espol.edu.ec>
2024-09-03 00:01:58 -0500

    Hover Tools

--- src/Pages/Incio/Skills/areaToolsSkils.jsx
+++ src/Pages/Incio/Skills/areaToolsSkils.jsx
@@ -5,7 +5,7 @@ function AreaToolsSkills() {
   return (
     <div className="flex flex-col place-content-center place-items-center text-center rounded-md w-full">
       <div className="flex flex-col w-[90%]">
-        <h1 className="text-5xl font-roboto-slab font-bold py-10">Tools and Languages</h1>
+        <h1 className="text-5xl font-roboto-slab font-bold py-10 max-sm:text-3xl">Tools and Languages</h1>
         <div className="rounded-lg flex place-content-center place-items-center flex-wrap gap-4">
           {info.tools &&
             info.tools.map((tool, index) => (
@@ -13,8 +13,8 @@ function AreaToolsSkills() {
             ))}
         </div>
         <div className="pb-20 pt-10" >
-          <h1 className="text-5xl font-roboto-slab font-bold py-10">Skills</h1>
-          <div className="flex place-content-center place-items-center gap-5">
+          <h1 className="text-5xl font-roboto-slab font-bold py-10 max-sm:text-3xl">Skills</h1>
+          <div className="flex place-content-center place-items-center gap-5 flex-wrap">
             {info.tools && info.areas.map((area, index) => (
               <SkillsSection area={area} />
             ))}
--- src/Pages/Incio/Skills/tool.jsx
+++ src/Pages/Incio/Skills/tool.jsx
@@ -1,7 +1,7 @@
 function Tool({ name_tool, image, link }) {
   return (
     <a href={link} target="_blank">
-      <div className="rounded-lg  bg-green-calid bg-opacity-40 flex place-content-center place-items-center py-2 px-4 gap-5 ">
+      <div className="rounded-lg  bg-green-calid bg-opacity-60 flex place-content-center place-items-center py-2 px-4 gap-5 hover:bg-opacity-15">
         <img src={image} alt={name_tool} className="w-10 h-10" />
         <p className="font-montserrat text-lg font-bold">{name_tool}</p>
       </div>
