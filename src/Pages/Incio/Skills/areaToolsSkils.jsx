f15bd7827cfeaa28a4b42b84b849a8587448ff2c
aszurita <nzuritag@fiec.espol.edu.ec>
2024-09-05 17:26:25 -0500

    Cleaning Project

--- src/Pages/Incio/Skills/areaToolsSkils.jsx
+++ src/Pages/Incio/Skills/areaToolsSkils.jsx
@@ -2,12 +2,12 @@ import { info } from "../../../data/HomeInfo.js";
 import Tool from "./Tool.jsx";
 import SkillsSection from "./Skills.jsx";
 import { ThemeContext } from "../Shared/ThemeContext.jsx";
-import {useContext} from 'react';
+import { useContext } from 'react';
 
 function AreaToolsSkills() {
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
   return (
-    <div className={`flex flex-col place-content-center place-items-center text-center w-full pt-2 ${darkMode?"bg-black":"bg-white"}`} id="Tools">
+    <div className={`flex flex-col place-content-center place-items-center text-center w-full pt-2 ${darkMode ? "bg-black" : "bg-white"}`} id="Tools">
       <div className="flex flex-col w-[90%]">
         <h1 className="text-5xl font-roboto-slab font-bold py-10 max-sm:text-3xl select-none">Tools and Languages</h1>
         <div className="rounded-lg flex place-content-center place-items-center flex-wrap gap-4">
