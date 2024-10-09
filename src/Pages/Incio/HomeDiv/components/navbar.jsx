b60c6edfa4331149d1b24e6a25430c9965a8f93a
aszurita <nzuritag@fiec.espol.edu.ec>
2024-10-09 08:56:31 -0500

    Navbar implementado con nav, revisar font usada

--- src/Pages/Incio/HomeDiv/components/navbar.jsx
+++ src/Pages/Incio/HomeDiv/components/navbar.jsx
@@ -1,57 +1,60 @@
 import { useState, useContext } from "react";
-import { Navbar } from "flowbite-react";
 import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
 import { ThemeContext } from "../../Shared/ThemeContext";
 
 function NavbarHome() {
   const [activeLink, setActiveLink] = useState("HOME");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <Navbar fluid rounded className={`fixed top-0 left-0 w-full z-50 ${darkMode ? "bg-black !text-white" : "bg-white"} `}>
-      <Navbar.Brand href="#Home" className="ml-10" onClick={() => setActiveLink("HOME")}>
-        <span className="font-roboto-slab self-center whitespace-nowrap text-xl dark:text-white text-green-calid">
-          AngeloZurita
-        </span>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="font-montserrat text-xl mr-10 lg:mr-40 select-none ">
-        <Navbar.Link
-          href="#AboutMe"
-          className={`hover:!text-green-calid ${activeLink === "ABOUT ME" ? "text-green-calid !border-b-2 !border-green-calid" : ""} mt-1`}
-          onClick={() => setActiveLink("ABOUT ME")}
-        >
-          ABOUT ME
-        </Navbar.Link>
-        <Navbar.Link
-          href="#Portfolio"
-          className={`hover:!text-green-calid ${activeLink === "PORTFOLIO" ? "text-green-calid !border-b-2 !border-green-calid" : ""} mt-1`}
-          onClick={() => setActiveLink("PORTFOLIO")}
-        >
-          PORTFOLIO
-        </Navbar.Link>
-        <Navbar.Link
-          href="#Tools"
-          className={`hover:!text-green-calid ${activeLink === "TOOLS" ? "text-green-calid !border-b-2 !border-green-calid" : ""} mt-1`}
-          onClick={() => setActiveLink("TOOLS")}
-        >
-          TOOLS
-        </Navbar.Link>
-        <div
-          className="flex items-center cursor-pointer max-sm:pt-2 max-sm:pr-4 max-sm:pb-2 max-sm:pl-3"
-          onClick={toggleDarkMode}
-        >
-          <span className="dark:text-white">
-            {!darkMode ? "DARK MODE" : "LIGHT MODE"}&nbsp;
+    <nav className={`fixed top-0 left-0 w-full z-50 ${darkMode ? "bg-black text-white" : "bg-white text-black"}`}>
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <a href="#Home" className="ml-4 lg:ml-10" onClick={() => setActiveLink("HOME")}>
+          <span className="font-roboto-slab self-center whitespace-nowrap text-xl text-green-calid">
+            AngeloZurita
           </span>
-          {darkMode ? (
-            <MdDarkMode size={20} />
-          ) : (
-            <MdOutlineDarkMode size={20} />
-          )}
+        </a>
+        <button
+          className="lg:hidden p-2 rounded-md focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <span className="sr-only">Toggle menu</span>
+          <svg
+            className={`w-6 h-6 ${darkMode ? 'text-white' : 'text-black'}`}
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+        </button>
+        <div className={`lg:flex lg:items-center lg:space-x-6 font-montserrat text-xl mr-4 lg:mr-40 select-none ${isMenuOpen ? `block absolute top-full left-0 right-0 ${darkMode ? "bg-black text-white" : "bg-white text-black"}` : 'hidden'}`}>
+          {['ABOUT ME', 'PORTFOLIO', 'TOOLS', 'DARK MODE'].map((item) => (
+            <a
+              key={item}
+              href={`#${item.toLowerCase().replace(' ', '')}`}
+              className={`block py-2 px-4 border-b border-gray-200 lg:inline-block lg:border-none hover:text-green-calid ${activeLink === item ? "text-green-calid border-green-calid" : ""}`}
+              onClick={() => {
+                setActiveLink(item);
+                if (item === 'DARK MODE') toggleDarkMode();
+                setIsMenuOpen(false);
+              }}
+            >
+              {item}
+              {item === 'DARK MODE' && (
+                <span className="ml-2 inline-block align-middle">
+                  {darkMode ? <MdDarkMode size={20} /> : <MdOutlineDarkMode size={20} />}
+                </span>
+              )}
+            </a>
+          ))}
         </div>
-      </Navbar.Collapse>
-    </Navbar>
+      </div>
+    </nav>
   );
 }
 
