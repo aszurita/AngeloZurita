58ccd10da642ed6440cbed57f95f5145a9343194
aszurita <nzuritag@fiec.espol.edu.ec>
2024-12-24 12:24:27 -0500

    Update Info, new experience

--- src/data/HomeInfo.js
+++ src/data/HomeInfo.js
@@ -1,5 +1,5 @@
 export let info = {
-  introductoryparagraph : "👋 Hi! <strong>I'm Angelo Saul Zurita Guerrero</strong>, a Computer Science student from Ecuador with a strong passion for programming and artificial intelligence. Currently, I serve as the Vice President of TAWS and a Teaching Assistant at ESPOL, where I lead research projects and support students in their learning journey. I am particularly interested in data science and computer vision, and I actively seek opportunities to apply my skills in real-world projects. I'm excited to connect and collaborate with others in the tech community! 🚀",
+  introductoryparagraph : "I am a Computer Science student passionate about Full Stack Development, Data Science, and Artificial Intelligence. With hands-on experience in projects like building the WiDS ESPOL website and analyzing data for impactful decisions, I aim to leverage technology to solve real-world problems. As Vice President of TAWS, I focus on fostering innovation and sharing knowledge within and beyond the community.",
   areas:[
     {
       name: "Data Science / AI",
